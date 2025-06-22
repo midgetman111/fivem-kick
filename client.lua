8918b874caee956cfca699730919b28e0acfdab3
midgetman111 <73799019+midgetman111@users.noreply.github.com>
2025-06-22 15:03:27 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,22 +1,26 @@
 RegisterNetEvent("kick:ilv-scripts:KickKidnapScene", function()
     local playerPed = PlayerPedId()
-    local coords = GetEntityCoords(playerPed)
+    local coords    = GetEntityCoords(playerPed)
 
-    local guardModel = joaat("s_m_m_armoured_02")
+    local guardModel = joaat("s_m_y_dealer_01")
     RequestModel(guardModel)
     while not HasModelLoaded(guardModel) do Wait(0) end
 
-    local guardPed = CreatePed(0, guardModel, coords.x + 1.0, coords.y, coords.z, 0.0, true, true)
+    local guardPed = CreatePed(0, guardModel, coords.x + 1.0, coords.y, coords.z,
+                               0.0, true, true)
     SetBlockingOfNonTemporaryEvents(guardPed, true)
     SetPedCanRagdoll(guardPed, false)
-    SetPedAsNoLongerNeeded(guardPed)
 
     local vanModel = joaat("burrito")
-    local van = GetClosestVehicle(coords, 15.0, vanModel, 70)
+    local van      = GetClosestVehicle(coords, 15.0, vanModel, 70)
+    local spawnedVan = false
+
     if not DoesEntityExist(van) then
         RequestModel(vanModel)
         while not HasModelLoaded(vanModel) do Wait(0) end
-        van = CreateVehicle(vanModel, coords.x + 3.0, coords.y + 1.0, coords.z, 0.0, true, false)
+        van = CreateVehicle(vanModel, coords.x + 3.0, coords.y + 1.0, coords.z,
+                            0.0, true, false)
+        spawnedVan = true
     end
 
     local scenePos = GetEntityCoords(van)
@@ -26,15 +30,28 @@ RegisterNetEvent("kick:ilv-scripts:KickKidnapScene", function()
     RequestAnimDict(animDict)
     while not HasAnimDictLoaded(animDict) do Wait(0) end
 
-    local scene = NetworkCreateSynchronisedScene(scenePos, sceneRot, 2, false, false, 1.0, 0, 1.0)
-    NetworkAddPedToSynchronisedScene(playerPed, scene, animDict, "ig_1_girl_drag_into_van", 8.0, -4.0, 1, 16, 0, 0)
-    NetworkAddPedToSynchronisedScene(guardPed, scene, animDict, "ig_1_guy2_drag_into_van", 8.0, -4.0, 1, 16, 0, 0)
-    NetworkAddEntityToSynchronisedScene(van, scene, animDict, "drag_into_van_burr", 1.0, 1.0, 1)
+    local scene = NetworkCreateSynchronisedScene(
+        scenePos, sceneRot, 2, false, false, 1.0, 0, 1.0
+    )
+
+    NetworkAddPedToSynchronisedScene(playerPed, scene, animDict,
+        "ig_1_girl_drag_into_van", 8.0, -4.0, 1, 16, 0, 0)
+    NetworkAddPedToSynchronisedScene(guardPed, scene, animDict,
+        "ig_1_guy2_drag_into_van", 8.0, -4.0, 1, 16, 0, 0)
+    NetworkAddEntityToSynchronisedScene(van, scene, animDict,
+        "drag_into_van_burr", 1.0, 1.0, 1)
 
     NetworkStartSynchronisedScene(scene)
 
     Wait(GetAnimDuration(animDict, "drag_into_van_burr") * 1000)
 
-    ClearPedTasks(playerPed)
+    ClearPedTasksImmediately(playerPed)
+
+    SetEntityAsMissionEntity(guardPed, true, true)
     DeleteEntity(guardPed)
+
+    if spawnedVan then
+        SetEntityAsMissionEntity(van, true, true)
+        DeleteEntity(van)
+    end
 end)
